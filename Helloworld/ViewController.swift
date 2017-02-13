a57d7ddf0dbbf600db9a465c35d8ad1813523da8
1234 <a1234@1234ui-MacBook-Pro.local>
2017-02-13 06:27:40 -0800

    Initial Commit

---
+++ Helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by 1234 on 2017. 2. 13..
+//  Copyright (c) 2017년 1234. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
